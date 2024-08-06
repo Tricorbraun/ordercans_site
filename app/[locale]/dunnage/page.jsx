fd80a429af4c9575e069540ebfd1b2c54db6ed95
Rhys <rhyswoody@gmail.com>
2024-08-06 14:33:17 -0700

    update dunnage page for mobile

--- app/[locale]/dunnage/page.jsx
+++ app/[locale]/dunnage/page.jsx
@@ -1,17 +1,18 @@
 'use client';
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 
 const Dunnage = () => {
   const [hiddenField, setHiddenField] = useState('');
   const onSubmit = () => {};
   const order = {};
+
   return (
     <section>
-      <div className="mx-36">
+      <div className="flex justify-center w-full mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-36">
         <iframe
-          width="900"
-          height="2000"
+          className="w-full max-w-3xl h-[1800px] sm:h-[1500px] md:h-[2000px]"
           src="https://app.smartsheet.com/b/form/b9242fc748cd43ebbe9b08027b11838c"
+          allowFullScreen
         ></iframe>
       </div>
     </section>
