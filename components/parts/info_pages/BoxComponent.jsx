67651b6178444c9f71c2058a74b9bb6ff58b3708
Rhys <rhyswoody@gmail.com>
2024-08-26 12:56:17 -0700

    add artwork pdf resources

--- components/parts/info_pages/BoxComponent.jsx
+++ components/parts/info_pages/BoxComponent.jsx
@@ -11,28 +11,28 @@ const BoxComponent = ({
   return (
     <div className="box-container">
       <h2 className="box-title">{title}</h2>
-      { rect1Text && (
+      {rect1Text && (
         <>
-      <a
-        href={rect1Link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`rectangle ${rectColor}`}
-      >
-        <p>{rect1Text}</p>
-      </a>
-      </>
+          <a
+            href={rect1Link}
+            download
+            rel="noopener noreferrer"
+            className={`rectangle ${rectColor}`}
+          >
+            <p>{rect1Text}</p>
+          </a>
+        </>
       )}
-    { rect2Text && (
-      <a
-        href={rect2Link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`rectangle ${rectColor}`}
-      >
-        <p>{rect2Text}</p>
-      </a>
-    )}
+      {rect2Text && (
+        <a
+          href={rect2Link}
+          download
+          rel="noopener noreferrer"
+          className={`rectangle ${rectColor}`}
+        >
+          <p>{rect2Text}</p>
+        </a>
+      )}
     </div>
   );
 };
