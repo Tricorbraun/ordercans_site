88709bd3862a1ae0ac21295094dec06f61cbd4ac
Rhys <rhyswoody@gmail.com>
2024-11-18 16:29:44 -0800

    initial commit for itemcreation

---
+++ app/[locale]/crs/itemcreation/page.jsx
@@ -0,0 +1,64 @@
+'use client';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const itemcreation = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <section className="vessel_form_wrapper">
+        <h1 className="head_text text_center w-full">
+          <span className="text-vp-yellow">What would you like to create?</span>
+        </h1>
+
+        <div className="grid md:grid-cols-2 gap-2 justify-items-center items-center px-6 py-6 md:px-14">
+          <div
+            onClick={() => handleClick('component')}
+            value="component"
+            className="type_container"
+          >
+            <img
+              src="/assets/icons/labelsIcon.svg"
+              alt="All In One Icon"
+              className="w-24 h-24"
+            />
+
+            <h1 className="font-barlowbold text-vp-orchid text-center text-2xl">
+              Component
+            </h1>
+
+            <div className="vessel_sub_text text-vp-orchid text-center px-4">
+              Create a single component (eg. label)
+            </div>
+          </div>
+
+
+        
+          <div
+            onClick={() => handleClick('finishedgood')}
+            value="finishedgood"
+            className="type_container"
+          >
+            <img
+              src="/assets/icons/allinonelogo.svg"
+              alt="All In One Icon"
+              className="w-24 h-24"
+            />
+
+            <h1 className="font-barlowbold text-vp-orchid text-center text-2xl">
+              Finished Good
+            </h1>
+
+            <div className="vessel_sub_text text-vp-orchid text-center px-4">
+              Create a finished good (eg. all in one can)
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default itemcreation;
