616d675003db064a9da050da6eb9c84b360c1de9
Rhys <rhyswoody@gmail.com>
2024-11-20 12:02:49 -0800

    add dunnage policy

--- app/[locale]/dunnage/page.jsx
+++ app/[locale]/dunnage/page.jsx
@@ -2,13 +2,21 @@
 import React, { useState } from 'react';
 
 const Dunnage = () => {
-  const [hiddenField, setHiddenField] = useState('');
-  const onSubmit = () => {};
-  const order = {};
-
   return (
     <section>
-      <div className="flex justify-center w-full mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-36">
+      <div>
+        <div className="gap-4 justify-items-center items-center mb-12 ml-22 mr-22 px-6 md:px-14">
+          <a
+            href="/assets/dunnagePolicy/dunnageEN.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full h-12 mb-12 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col space-y-4 text-white hover:text-tb-brown hover:bg-tb-green/50 bg-tb-green cursor-pointer"
+          >
+            Click Here to read our dunnage policy
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center w-full ">
         <iframe
           className="w-full max-w-3xl h-[1800px] sm:h-[1500px] md:h-[2000px]"
           src="https://app.smartsheet.com/b/form/b9242fc748cd43ebbe9b08027b11838c"
